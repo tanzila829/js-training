8d1e75231f77459923d4f02aa62488e83a13c56c
tanzila akter <aktanzila3@gmail.com>
2022-10-18 16:47:46 +0600

    first

---
+++ node.js
@@ -0,0 +1,8 @@
+var array=[];
+var divisio=(0);
+for(i=1; i<=100; i++){
+    array.push(i);
+    division/=(i);
+}
+console.log(array);
+console.log(division);
---
+++ tanzu.js
@@ -0,0 +1,5 @@
+var array=[];
+for(i=1; i<=1000; i++){
+    array.push(i);
+}
+console.log(array);
